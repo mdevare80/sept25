f7f660dbbc63fb912a1933b76cccc3f8ed85c17d
Atharv Kaveeshwar <62415511+Kaveeshwarji@users.noreply.github.com>
2021-01-15 09:34:04 +0530

    Add files via upload

---
+++ 1to5.java
@@ -0,0 +1,9 @@
+class display{
+    public static void main(String[] args){
+        int i=1, n=5;
+        for(i=1; i<=n; i++)
+        {
+            System.out.println(i + " ");
+        }
+    }
+}
